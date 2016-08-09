505002e8e8e73946d0c6013f1470d9546e84c1aa
Agustin Albertengo <agualbbus@users.noreply.github.com>
2016-08-09 02:43:25 -0300

    Update class-s3-uploads.php

--- inc/class-s3-uploads.php
+++ inc/class-s3-uploads.php
@@ -123,8 +123,8 @@ class S3_Uploads {
 
 		$bucket = strtok( $this->bucket, '/' );
 		$path   = substr( $this->bucket, strlen( $bucket ) );
-
-		return apply_filters( 's3_uploads_bucket_url', 'https://' . $bucket . '.s3.amazonaws.com' . $path );
+		$s3_url = defined('S3_BASE_URL') ? S3_BASE_URL . $path : 'https://' . $bucket . '.s3.amazonaws.com' . $path;
+		return apply_filters( 's3_uploads_bucket_url', $s3_url );
 	}
 
 	/**
